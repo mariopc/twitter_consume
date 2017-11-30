d7f25c0a93ee67c3a2b2bdae186ba1a85580bdfb
Mario Peña <mario.pc2009@gmail.com>
2017-11-30 15:52:37 -0300

    Primer Release

---
+++ index.js
@@ -0,0 +1,28 @@
+var Twitter = require('twitter');
+var moment = require('moment');
+var fs = require('fs');
+
+var configObj = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+var client = new Twitter({
+    consumer_key: configObj.consumer_key,
+    consumer_secret: configObj.consumer_secret,
+    access_token_key: configObj.access_token_key,
+    access_token_secret: configObj.access_token_secret
+});
+
+let now = moment().format('YYYY-MM-DD');
+
+let query = `${configObj.hashtag} since:${now}`
+
+client.get('search/tweets', {q: query, result_type: 'recent', lang: 'es'}, (error, tweets, response) => {    
+    if (error)
+        console.log(error);
+    for (tweet in tweets.statuses) {
+        var reg = new RegExp(configObj.words, "i");
+        if (tweets.statuses[tweet].text.search(reg) != -1) {
+            let time = moment(tweets.statuses[tweet].created_at, "dd MMM DD HH:mm:ss ZZ YYYY").format("YYYY-MM-DD HH:mm:ss ZZ");
+            console.log('[' + time + '] - ' + tweets.statuses[tweet].text);
+        }
+    }
+});
